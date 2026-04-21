40b53315dec60ae8866e324a963b5060a2867a6c
Asher Pardel <gracepardel7@gmail.com>
2026-04-20 22:48:58 -0400

    secured the vault

---
+++ index.js
@@ -0,0 +1,8 @@
+let code01;
+code01 = 7 + 3;
+let code02 = 100 / 2 - 10;
+let code03 = 39 % 50;
+let message = "The vault has been secured. The combination is:";
+let codeA = code01 + "-" + code02 + "-" + code03;
+let codeB = `${code01}-${code02}-${code03}`;
+console.log(message, codeA, codeB);
